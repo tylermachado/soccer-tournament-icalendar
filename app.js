944a74da81aef0f987854d2131945b146d476fb1
tylermachado <3586466+tylermachado@users.noreply.github.com>
2019-05-13 14:11:09 -0400

    automatically go over each file

--- app.js
+++ app.js
@@ -1,47 +1,60 @@
+const path = require('path');
 const fs = require('fs');
 const ics = require('ics');
 const csv = require('csv-parser');
 
-const matches = [];
 
-fs.createReadStream('data/copaamerica.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    var match = new Object();
-    var matchMonth = parseInt(row.date.split("-")[1]);
-    var matchDate = parseInt(row.date.split("-")[2]);
-    var matchHour = (parseInt(row.time.split(":")[0]) - (parseFloat(row.tz.split("C")[1])));
+const directoryPath = path.join(__dirname, 'data');
 
-    if (matchHour > 23) {
-        matchHour = matchHour-24;
-        matchDate = matchDate + 1;
-        if (matchDate > 30) {
-            matchDate = matchDate - 30;
-            matchMonth = matchMonth + 1;
-        }
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
     }
 
-    console.log(matchHour);
+    files.forEach(function (file) {
+        const matches = [];
 
-    match.title = row.home_team + " v " + row.away_team;
-    match.start = [parseInt(row.date.split("-")[0]), matchMonth, matchDate, matchHour, parseInt(row.time.split(":")[1])];
-    match.duration = {hours: 2};
-    match.description = row.phase + "\nMatch " + row.id;
+        var filename = file.split(".")[0];
+        var initials = filename.match(/\b(\w)/g).join('');
 
-    matches.push(match);
-  })
-  .on('end', () => {
-      const { error, value } = ics.createEvents(matches)
+        fs.createReadStream(directoryPath + "/" + file)
+          .pipe(csv())
+          .on('data', (row) => {
+            var match = new Object();
+            var matchMonth = parseInt(row.date.split("-")[1]);
+            var matchDate = parseInt(row.date.split("-")[2]);
+            var matchHour = (parseInt(row.time.split(":")[0]) - (parseFloat(row.tz.split("C")[1])));
 
-      if (error) {
-        console.log(error)
-        return
-      } else {
-        console.log('.csv file processed');
-      }
+            if (matchHour > 23) {
+                matchHour = matchHour-24;
+                matchDate = matchDate + 1;
+                if (matchDate > 30) {
+                    matchDate = matchDate - 30;
+                    matchMonth = matchMonth + 1;
+                }
+            }
 
-      fs.writeFile('dist/copaamerica.ics', value, (err) => {
-        if (err) throw err;
-        console.log('.ics calendar file saved');
-      });
-  });
+            match.title = "[" + initials + "] " + row.home_team + " v " + row.away_team;
+            match.start = [parseInt(row.date.split("-")[0]), matchMonth, matchDate, matchHour, parseInt(row.time.split(":")[1])];
+            match.duration = {hours: 2};
+            match.description = row.phase + "\nMatch " + row.id;
+
+            matches.push(match);
+          })
+          .on('end', () => {
+              const { error, value } = ics.createEvents(matches)
+
+              if (error) {
+                console.log(error)
+                return
+              } else {
+                console.log('.csv file processed');
+              }
+
+              fs.writeFile('dist/' + filename + '.ics', value, (err) => {
+                if (err) throw err;
+                console.log('.ics calendar file saved');
+              });
+          });
+    });
+});
